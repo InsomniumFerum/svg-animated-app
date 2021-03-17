687ede130458da35a84433695935d029120e4a71
Furkan Çat <75113766+InsomniumFerum@users.noreply.github.com>
2021-03-17 16:40:34 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,82 @@
+"use-strict";
+
+const app = () => {
+  const song = document.querySelector(".song");
+  const play = document.querySelector(".play");
+  const outline = document.querySelector(".moving-outline circle");
+  const video = document.querySelector(".vid-container video");
+
+  // Sounds
+  const sounds = document.querySelectorAll(".sound-picker button");
+
+  // Time Display
+  const timeDisplay = document.querySelector(".time-display");
+  const timeSelect = document.querySelectorAll(".time-select button");
+  // Get the length of the outline
+  const outlineLength = outline.getTotalLength();
+  //console.log(outlineLength);
+  // Duration
+  let fakeDuration = 60;
+
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+
+  // Pick different sounds
+  sounds.forEach((sound) => {
+    sound.addEventListener("click", function () {
+      song.src = this.getAttribute("data-sound");
+      video.src = this.getAttribute("data-video");
+      checkPlaying(song);
+    });
+  });
+
+  // Play Sound
+  play.addEventListener("click", () => {
+    checkPlaying(song);
+  });
+
+  // Select Sound
+  timeSelect.forEach((option) => {
+    option.addEventListener("click", function () {
+      fakeDuration = this.getAttribute("data-time");
+      timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+        fakeDuration % 60
+      )}0`;
+    });
+  });
+
+  const checkPlaying = (song) => {
+    if (song.paused) {
+      song.play();
+      video.play();
+      play.src = "./svg/pause.svg";
+    } else {
+      song.pause();
+      video.pause();
+      play.src = "./svg/play.svg";
+    }
+  };
+
+  song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    let elapsedTime = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsedTime % 60);
+    let minutes = Math.floor(elapsedTime / 60);
+
+    // Animate the circle
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+    // Animate text
+    timeDisplay.textContent = `${minutes}:${seconds}`;
+
+    if (currentTime >= fakeDuration) {
+      song.pause();
+      song.currentTime = 0;
+      play.src = "./svg/play.svg";
+      video.pause();
+      timeDisplay.textContent = fakeDuration;
+    }
+  };
+};
+
+app();
